46d7200ca54db47fb00d3434543ab3b07872df5f
Oscar Ossa <oskrlex17@gmail.com>
2024-02-12 15:03:52 -0500

    add resources and interfaces endpoints

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import { RouterOSClient } from "routeros-client";
 import * as dotenv from "dotenv";
+import authTokenMiddleware from "./middlewares/authTokenMiddleware.js";
 
 dotenv.config();
 
@@ -11,17 +12,17 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*"
+    origin: "*",
   },
 });
 
 io.use((socket, next) => {
   const token = socket.handshake.auth.token;
-  if (token == process.env.TOKEN) {
+  if (token == process.env.SOCKET_TOKEN) {
     next();
   } else {
     console.log(`Socket ${socket.id} unauthorized`);
-    next(new Error('unauthorized'))
+    next(new Error("unauthorized"));
   }
 });
 
@@ -72,7 +73,7 @@ io.on("connection", (socket) => {
                 });
               }
             }
-          })
+          });
       })
       .catch((err) => {
         console.log(err);
@@ -101,6 +102,77 @@ io.on("connection", (socket) => {
     }
   });
 });
+
+app.use(express.json());
+app.use(authTokenMiddleware)
+
+app.get("/interfaces",(req, res) => {
+  const { host } = req.body;
+  
+  const api = new RouterOSClient({
+    host: host,
+    user: process.env.API_USER,
+    password: process.env.API_PASSWORD,
+  });
+
+  api.connect()
+    .then((client) => {
+      const interfaceMenu = client.menu("/interface print");
+      interfaceMenu.get()
+        .then((data) => {
+          
+          const result = data.filter(obj => obj.type !== "pppoe-in" && obj.type !== "l2tp-in" && obj.type !== "sstp-out");
+
+          res.status(200).send(result)
+
+        })
+        .catch((e) => {
+          console.log(e);
+          res.status(500).send("Error al obtener los datos de la interfaz.");
+        })
+        .finally(() => {
+          api.close()
+        });
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(500).send("Error al conectar con el dispositivo.");
+    });
+});
+
+app.get("/resources",(req, res) => {
+  const { host } = req.body;
+  
+  const api = new RouterOSClient({
+    host: host,
+    user: process.env.API_USER,
+    password: process.env.API_PASSWORD,
+  });
+
+  api.connect()
+    .then((client) => {
+      const interfaceMenu = client.menu("/system/resource print");
+      interfaceMenu.get()
+        .then((data) => {
+
+          res.status(200).send(data[0])
+        })
+        .catch((e) => {
+          console.log(e);
+          res.status(500).send("Error al obtener los recursos.");
+        })
+        .finally(() => {
+          api.close()
+        });
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(500).send("Error al conectar con el dispositivo.");
+    });
+
+  
+});
+
 
 server.listen(process.env.PORT, () => {
   console.log(`listening on *:${process.env.PORT}`);
